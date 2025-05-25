b48ae9fa4ba464fabb5c71a012e9bb0f76649e56
alnomaanbd365 <khanromaan31@gmail.com>
2025-05-26 00:09:50 +0600

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '7875865850:AAEB-sf0jCurPNbaoOvn4wHW_bqUokib1EY',
-    TELEGRAM_GROUP_ID: process.env.TELEGRAM_GROUP_ID || '-4649261860',
-    PREMIUMY_API_KEY: process.env.PREMIUMY_API_KEY || 'BYIl-WDHTNm-klFJflcuEQ',
-    PREMIUMY_API_BASE_URL: 'https://client.premiumy.net/api'
-}; 
