217d8d6508765e0b3f7e71dac13048e598c1dbc3
alnomaanbd365 <khanromaan31@gmail.com>
2025-05-26 00:11:16 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '7875865850:AAEB-sf0jCurPNbaoOvn4wHW_bqUokib1EY',
+    TELEGRAM_GROUP_ID: process.env.TELEGRAM_GROUP_ID || '-4649261860',
+    PREMIUMY_API_KEY: process.env.PREMIUMY_API_KEY || 'kHU3KkA1TLKN9vswZJkhKg',
+    PREMIUMY_API_BASE_URL: 'https://client.premiumy.net/api'
+}; 
